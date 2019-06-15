942b6e25ea019715b35a4542e8c03f0720a79251
AlexandrMX <sanjaruv@inbox.lv>
2019-06-15 12:22:41 +0300

    Cookie works!

--- chat-project/src/components/App/App.js
+++ chat-project/src/components/App/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import Header from '../Header/header';
 import Content from '../Content/content';
-import dbRef from '../../lib/dbRef';
+// import dbRef from '../../lib/dbRef';
 import store from '../../lib/store';
+import { initialiseListeners } from '../../lib/profile/actions'
 
-dbRef
-  .once('value')
-  .then(data => data.val())
-  .then(data => store.dispatch({ type: 'DATA_LOADED', data }));
+// dbRef
+//   .once('value')
+//   .then(data => data.val())
+//   .then(data => store.dispatch({ type: 'DATA_LOADED', data }));
 
+store.dispatch(initialiseListeners());
 
 const App = () => {
   return (
--- chat-project/src/lib/profile/actions.js
+++ chat-project/src/lib/profile/actions.js
@@ -1,7 +1,8 @@
 import dbRef from '../dbRef';
 import { addChat } from '../chatList/actions';
 import { addProfile } from '../profileList/actions';
-import firebase, { firestore } from 'firebase';
+import firebase from 'firebase';
+import Cookie from 'js-cookie';
 
 export const runLogin = (username, password) => ({ getState, dispatch }) => {
     getState().profile.id = username ? username : getState().profile.id; //dont do this, CHANGE ME
@@ -16,31 +17,43 @@ export const authDone = ({ id, email }) => ({
     user: { id, email }
 });
 
-export const authUser = ( username, password ) => ({ dispatch }) => {
-    console.log(username, password)   
+export const authUser = (username, password) => ({ dispatch }) => {
+    console.log(username, password)
     firebase
         .auth()
         .signInWithEmailAndPassword(username, password)
         .then(firebaseUser => {
             console.log(firebaseUser.user, "PPPPPP")
             const { uid, email } = firebaseUser.user;
-           
-            // Cookie.set('chat_user', uid);
-            // Cookie.set('chat_user_email', email);
-            dispatch(authDone({id:uid, email }));
-            dispatch(addChat(uid))
-            dispatch(addProfile(uid))
-            // return { id, email };
+
+            Cookie.set('chat_user', uid);
+            Cookie.set('chat_user_email', email);
+
+            return { uid, email };
         })
-        // .then(() => dispatch(initialiseListeners()));
+        .then(() => dispatch(initialiseListeners()));
 
     return { type: 'AUTH_USER' };
 };
-export const setUsername = username =>  {
-    return { type: 'SET_USERNAME', username };
-}
+// export const setUsername = username =>  {
+//     return { type: 'SET_USERNAME', username };
+// }
 
-export const setPassword = password => {
-    return {type: 'SET_PASSWORD', password}
-}
+// export const setPassword = password => {
+//     return {type: 'SET_PASSWORD', password}
+// }
+
+export const initialiseListeners = () => ({ dispatch }) => {
+    const uid = Cookie.get('chat_user');
+    const email = Cookie.get('chat_user_email');
+    console.log ('!!!!!!', uid )
+    if (uid) {
+        console.log ('????????', uid )
+        dispatch(authDone({ id: uid, email }));
+        dispatch(addChat(uid))
+        dispatch(addProfile(uid))
+    }
+
+    return { type: 'INITIALISE_LISTENERS' };
+};
 
