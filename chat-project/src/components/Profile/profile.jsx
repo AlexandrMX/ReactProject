fa1d32799c937784cde507c5f145ea950fa01c71
unknown <martins.petters@gmail.com>
2019-06-17 16:13:21 +0300

    profile status display

--- chat-project/src/components/Profile/profile.jsx
+++ chat-project/src/components/Profile/profile.jsx
@@ -3,20 +3,46 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { profileSelect } from "../../lib/profileList/actions";
 import { ListGroupItem } from 'reactstrap';
+import Badge from '@material-ui/core/Badge';
 import Avatar from 'react-avatar';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    dot: {
+        height: '12px',
+        width: '12px',
+        top: '30px',
+        right: '8px',
+        border: 'solid 2px'
+    },
+    active: {
+        background: 'green',
+        'border-color': 'white'
+    },
+    inactive: {
+        display: 'none'
+    }
+
+
+});
 
 const Profile = ({ profile, onProfileSelect }) => {
+    const classes = useStyles();
     return (
         <ListGroupItem className="profile">
             <div onClick={onProfileSelect(profile)} >
-                <Avatar
-                    name={profile.displayName}
-                    color={profile.color}
-                    src={`data:image/jpeg;base64,${profile.avatar}`}
-                    round={true}
-                    size={30}
-                    style={{ margin: 5 }}
-                />
+                <Badge 
+                    variant="dot" 
+                    classes={{ dot: classes.dot + ' ' + (profile.status === 'active' ? classes.active: classes.inactive)}}>
+                    <Avatar
+                        name={profile.displayName}
+                        color={profile.color}
+                        src={`data:image/jpeg;base64,${profile.avatar}`}
+                        round={true}
+                        size={30}
+                        style={{ margin: 5 }}
+                    />
+                </Badge>
                 {profile.displayName}
             </div>
         </ListGroupItem >
